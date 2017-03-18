135a3e37da2b30f8d1ba6085b538f17dc12a2eba
Christopher H. Casebeer <christopher@chc.name>
2017-03-18 19:53:48 -0400

    Retry query logic to make discovery more robust
    Retry PTR query for _googlecast._tcp.local default three times to make
    it more likely that we will quickly discover all devices on the network.
    
    Query for TXT, SRV, and A records as needed if the are missing for any
    Chromecasts discovered so far via PTR records.
    
    In case some Cast devices don't include the necssary TXT, SRV, and A
    records for DNS-SD in the ADDITIONAL section of the initial PTR
    response, check for any missing data after each response packet has been
    processed and request any records still needed.
    
    Adds lock flags to prevent running extra queries when queries are
    already in flight and to prevent running any queries after the close()
    method has been called.
    
    Adds options to set retry backoff time and number of PTR queries to run.
    
    Shortcomings:
    - Does not account for IPv6-only Cast devices, since it requires A
      records. n.b. At the moment Cast devices do not seem to even offer
      AAAA records, but they have used v6 in the past, and the advertized
      local connection hostnames could easily be v6 only (e.g. SLAAC) in
      the future.

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var defaults = {
   ttl: 5000,
   service_name: '_googlecast._tcp.local',
   service_type: 'PTR',
-  mdns: {}
+  mdns: {},
+  retry_timeout: 1000,
+  search_retry_count: 3
 };
 
 module.exports = function(opts, cb) {
@@ -22,10 +24,19 @@ module.exports = function(opts, cb) {
 
   var m = mdns(opts.mdns);
   var returnedDevices = {};
+  var retryLock = false;
+  var queryLock = false;
 
   var timer = setTimeout(function() {
     close();
   }, opts.ttl);
+
+  var doQuery = function (opts) {
+    // only run queries if we havnen't close()ed down yet
+    if (!queryLock) {
+      m.query(opts);
+    }
+  };
 
 
   var processAnswer = function (answer) {
@@ -59,8 +70,61 @@ module.exports = function(opts, cb) {
       // return any and all chromecasts that have IPs
       returnDevices(chromecasts.reachable());
     }
-
   };
+
+  var requestAdditional = function () {
+    // Ensure we have SRV, TXT, and A records for all discovered Chromecasts
+    // Request the data in case it wasn't in the ADDITIONAL section of the initial response
+    var i,
+      device,
+      questions = [],
+      devices;
+    // don't bother checking if we're not allowed to send queries
+    if (!retryLock && !queryLock) {
+      //console.log('Checking for data completeness...');
+      devices = chromecasts.all();
+      for (i = 0; i < devices.length; i++) {
+        device = devices[i];
+        //console.log("Device is %s", device.name);
+        if (device) {
+          if (!device.uuid) {
+            // missing UUID means we didn't get the TXT record
+            questions.push({
+              name: device.name,
+              type: 'TXT'
+            });
+          }
+          if (!device.ipv4) {
+            // missing A record
+            if (!device.host) {
+              // missing SRV record
+              questions.push({
+                name: device.name,
+                type: 'SRV'
+              });
+            } else {
+              // we have a have hostname from the SRV record, just need the A
+              questions.push({
+                name: device.host,
+                type: 'A'
+              });
+            }
+          }
+        }
+      }
+      if (questions.length > 0) {
+        //console.log('Extra questions: %s', questions.map(function (q) { return q.name + ' IN ' + q.type; }));
+        doQuery({ questions: questions });
+
+        // set lock to prevent another re-query for opts.retry_timeout milliseconds (default 1000 ms)
+        retryLock = true;
+        setTimeout(function () {
+          retryLock = false;
+          requestAdditional();
+        }, opts.retry_timeout);
+      }
+    }
+  }
 
   var onResponse = function(response) {
     var i;
@@ -72,6 +136,8 @@ module.exports = function(opts, cb) {
       processAnswer(response.additionals[i]);
     }
 
+    requestAdditional();
+
     // check to see if we have enough data to fulfill request each 
     // time we finish processing a new incoming mDNS packet
     handleCallback();
@@ -79,14 +145,24 @@ module.exports = function(opts, cb) {
 
   m.on('response', onResponse);
 
-  m.query({
-    questions:[{
-      name: opts.service_name,
-      type: opts.service_type
-    }]
-  });
+  var doPTRSearch = function (queries_remaining) {
+    // Run opts.search_retry_count queries (default 3 queries) for chromecasts
+    // at retry_timeout millisecond (default 1000 ms) intervals
+    // Raises chances of quickly seeing Cast devices if the first query was missed.
+    doQuery({
+      questions:[{
+        name: opts.service_name,
+        type: opts.service_type
+      }]
+    });
+    if (queries_remaining > 1) {
+      setTimeout(function () { doPTRSearch(queries_remaining - 1); }, opts.retry_timeout);
+    }
+  };
+  doPTRSearch(opts.search_retry_count);
 
   var close = function() {
+    queryLock = true;
     m.removeListener('response', onResponse);
     clearTimeout(timer);
     m.destroy();
