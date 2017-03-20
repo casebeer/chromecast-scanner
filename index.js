5c02d77c11fe8682a4ce3829052fa790be1959bc
Christopher H. Casebeer <christopher@chc.name>
2017-03-20 06:10:13 -0400

    restore old API

--- index.js
+++ index.js
@@ -44,14 +44,14 @@ module.exports = function(opts, cb) {
     mdnsCache.add(answer);
   };
 
-  var returnDevices = function (devices) {
+  var returnDevices = function (devices, response) {
     var device, i;
     for (i = 0; devices && i < devices.length; i++) {
       device = devices[i];
       // return each valid device once only
       if (device && !returnedDevices[device.name]) {
         // NB API change – passing friendly name and full CC details here, not A record and raw Response
-        cb(null, { name:device.friendly_name, data:device.ipv4 }, device);
+        cb(null, xtend(device.a, { name:device.friendly_name, device:device }), response);
         returnedDevices[device.name] = true;
       }
     }
@@ -59,16 +59,16 @@ module.exports = function(opts, cb) {
 
   // If we have enough mDNS data gathered to fulfill the caller's
   // request, respond via the callback funciton.
-  var handleCallback = function () {
+  var handleCallback = function (response) {
     if (typeof opts.filter === 'function') {
       // filter discoverd chromecasts with the provided function
-      returnDevices(chromecasts.filter(opts.filter));
+      returnDevices(chromecasts.filter(opts.filter), response);
     } else if (opts.name) {
       // search the discovered chromecasts for specified friendly name
-      returnDevices([chromecasts.named(opts.name)]);
+      returnDevices([chromecasts.named(opts.name)], response);
     } else {
       // return any and all chromecasts that have IPs
-      returnDevices(chromecasts.reachable());
+      returnDevices(chromecasts.reachable(), response);
     }
   };
 
@@ -140,7 +140,7 @@ module.exports = function(opts, cb) {
 
     // check to see if we have enough data to fulfill request each 
     // time we finish processing a new incoming mDNS packet
-    handleCallback();
+    handleCallback(response);
   };
 
   m.on('response', onResponse);
